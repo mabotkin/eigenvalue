91d4915f268fc305d055cc92803c43bedb1df887
Alex Wang <alex3763@gmail.com>
2017-02-11 22:13:47 -0500

    basic rooms

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, redirect, render_template
+from flask_socketio import SocketIO
+import random
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+GAME_ID_LENGTH=6
+
+games = []
+
+def genID(length):
+	chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"
+	ID = ""
+	for i in range(length):
+		ID += random.choice(chars)
+	return ID
+
+@app.route("/")
+def root():
+    return app.send_static_file('static/index.html')
+
+@app.route("/newgame")
+def newGame():
+	ID = genID(GAME_ID_LENGTH)
+	games.append(ID)
+	return redirect("/games/" + ID)
+
+@app.route("/games/<game_ID>")
+def gameRoom(game_ID):
+	return render_template("game.html", gameID = game_ID)
+	
+
+if __name__ == "__main__":
+    socketio.run(app,port=5001, debug=True)
